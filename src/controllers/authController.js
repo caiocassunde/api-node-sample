0ca66f7c801fcda1523e440cf10c29424b8234b6
Caio Sabino <caiosabino@MacBook-Air-de-Caio.local>
2019-08-06 01:08:31 -0300

    Inclusão dos métodos GET e DELETE (em desenvolvimento)

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -4,6 +4,21 @@ const User = require('../models/User');
 
 const router = express.Router();
 
+router.get('/register/get-email', async (req, res) => {
+    try{
+        const {email} = req.query
+        var ret = await User.findOne({email});
+        if(ret){
+            return res.send({ret});
+        }else{
+            return res.status(400).send({error: 'Client not found in DB'})
+        }
+    }catch(err){
+        return res.status(400).send({error: 'GET Failed'});
+    }
+});
+
+
 router.post('/register', async (req, res) => {
     try{
         const {email} = req.body;
@@ -18,6 +33,22 @@ router.post('/register', async (req, res) => {
         return res.send({user});     
     }catch (err){
         return res.status(400).send({error: 'Registration failed'});
+    }
+});
+
+router.delete('/register/delete-email/:email', async (req, res) => {
+    try{
+        const {email} = req.params
+        console.log({email});
+        var ret = await User.findOne({email});
+        if(ret){
+            await User.deleteOne({email});
+            return res.send({message: 'Client Deleted'});
+        }else{
+            return res.status(400).send({error: 'Client not found in DB'})
+        }
+    }catch(err){
+        return res.status(400).send({error: 'DELETE Failed'});
     }
 });
 
